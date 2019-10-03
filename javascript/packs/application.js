0739ba3c71bb220b63256a78dac98ff861ad3ef8
Elisa <codersquirrelbln@gmail.com>
2019-10-03 17:41:23 +0200

    add flatpickr in html file

--- javascript/packs/application.js
+++ javascript/packs/application.js
@@ -1,6 +1,10 @@
-// import "bootstrap";
+import "bootstrap";
 import initializeFlatpickr from "../plugins/flatpickr";
 // import { initSweetalert } from '../plugins/init_sweetalert';
 // import "flatpickr/dist/flatpickr.min.css"; // Note this is important!
+
+import "flatpickr/dist/flatpickr.min.css"; // Note this is important!
+import '../components/interest_tiles_selection.js';
+
 
 initializeFlatpickr();
