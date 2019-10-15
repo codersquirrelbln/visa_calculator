d465f3ef4577875d3f8c7aef45fa8c6aecc539a5
Elisa <codersquirrelbln@gmail.com>
2019-10-15 11:34:52 +0200

    moving function out of callback

--- javascript/main.js
+++ javascript/main.js
@@ -3,6 +3,7 @@ const addBtn = document.querySelector('.add-btn');
 const timeFrame = document.querySelector('#timePeriod');
 const maxDays = document.querySelector('#maxDays');
 const submitBtn = document.querySelector('.submit-btn');
+
 let entryDateNum = 1;
 let exitDateNum = 1;
 let allNewEntries = [];
@@ -20,13 +21,16 @@ time.flatpickr({
 
 let fpEntry = flatpickr('#entryDate1', {});
 let fpExit = flatpickr('#exitDate1', {});
-// let addedDays = 0;
-// console.log(addedDays);
-// let allAddedDaysRounded = 0;
-// console.log(allAddedDaysRounded);
 let newFpEntry;
 let newFpExit;
 let newFieldPairs = 0;
+
+// function to calculate the timeFrame input to the entryDate
+function addDays(date, days) {
+  let result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+}
 
 const addFields = function (event) {
   event.preventDefault();
@@ -80,9 +84,7 @@ const addFields = function (event) {
   console.log('newFpEntry');
 }
 
-
 /* adding another function that will check how many new entries there are (.include?) and count them, get the number to loop through the days count
-
 */
 
 // adding eventListener to the Add Trip button, callbackfunction = addField
@@ -92,68 +94,61 @@ addBtn.addEventListener('click', addFields);
 could limit the dates you can pick by using first entry + timeframe and cancel out every date after that
 using lastExit as limit on datepickr */
 
+
 // start calculations when the submit button is clicked
 submitBtn.addEventListener('click', event => {
 
   // prevents the fields to be 'filled' with invalid input elements
   event.preventDefault();
-  console.log(amountAllDays);
-  // if (amountAllDays.value != 0) {amountAllDays = 0};
 
   // defining variables and assigning them to values you can use in calculation
   let maxDaysValue = maxDays.value;
-
   let timeFrameValue = parseInt(timeFrame.value);
   let text;
-  const firstEntryDate = fpEntry.selectedDates[0];
-
-  const firstExitDate = fpExit.selectedDates[0];
-
+  let firstEntryDate = fpEntry.selectedDates[0];
+  let firstExitDate = fpExit.selectedDates[0];
 
   /* calculating the days of first trip, adding one day, since the entry day as
   well as exit day count as one full day each */
-  const amountFirstDays = ((firstExitDate - firstEntryDate) / (60*60*24*1000)) + 1;
-  const amountFirstDaysRounded = Math.floor(amountFirstDays);
-  // let amountAllDays;
+  let amountFirstDays = ((firstExitDate - firstEntryDate) / (60*60*24*1000)) + 1;
+  let amountFirstDaysRounded = Math.floor(amountFirstDays);
 
-  /* check if there are new fields added or the calculation is limited to onw trip
-  if there is an element in the DOM that has the id = entryDate2,
+    /* check if there are new fields added or the calculation is limited to onw trip
+    if there is an element in the DOM that has the id = entryDate2,
     the value will be assigned to the new entry variables and the amount of days
     bewteen the dates will be calculated */
     let addedDays = 0;
     let allAddedDaysRounded = 0;
 
-  if (document.querySelector('#entryDate2')){
-    let newEntryDate;
-    let newExitDate;
+    if (document.querySelector('#entryDate2')){
+      let newEntryDate;
+      let newExitDate;
 
-    for (let i = 0; i < newFieldPairs; i ++) {
-      let entries = allNewEntries[i];
-      console.log(`entries: ${entries}`);
-      newEntryDate = entries.selectedDates[0];
-      console.log(`new entry date = ${newEntryDate}`);
-      let exits = allNewExits[i];
+      for (let i = 0; i < newFieldPairs; i ++) {
+        let entries = allNewEntries[i];
+        // console.log(`entries: ${entries}`);
+        newEntryDate = entries.selectedDates[0];
+        // console.log(`new entry date = ${newEntryDate}`);
+        let exits = allNewExits[i];
 
-      newExitDate = exits.selectedDates[0];
-      console.log(`new exit date = ${newExitDate}`);
+        newExitDate = exits.selectedDates[0];
+        // console.log(`new exit date = ${newExitDate}`);
 
-      /* calculating the days, adding one day, since the entry day as well
-      as exit day count as one full day each */
-      addedDays = ((newExitDate - newEntryDate) / (60*60*24*1000)) + 1;
-      console.log(`added days = ${addedDays}`);
+        /* calculating the days, adding one day, since the entry day as well
+        as exit day count as one full day each */
+        addedDays = ((newExitDate - newEntryDate) / (60*60*24*1000)) + 1;
+        // console.log(`added days = ${addedDays}`);
 
-      /*using Math.floor to round down the days, since it would otherwise
-      take hours into account */
-      addedDaysRounded = Math.floor(addedDays);
-      console.log(`added days rounded = ${addedDaysRounded}`);
+        /*using Math.floor to round down the days, since it would otherwise
+        take hours into account */
+        addedDaysRounded = Math.floor(addedDays);
+        // console.log(`added days rounded = ${addedDaysRounded}`);
 
-      // adding result to the variable that will later be used to calculate all days
-      allAddedDaysRounded += addedDaysRounded;
-      console.log(`all added days rounded = ${allAddedDaysRounded}`);
-  // console.log('break');
+        // adding result to the variable that will later be used to calculate all days
+        allAddedDaysRounded += addedDaysRounded;
+        // console.log(`all added days rounded = ${allAddedDaysRounded}`);
+      }
     }
-
-  }
 
   if (!document.querySelector('#entryDate2')){
     // If there is only one trip, amountAllDays = amountFirstDaysRounded
@@ -163,12 +158,7 @@ submitBtn.addEventListener('click', event => {
     amountAllDays = amountFirstDaysRounded  + allAddedDaysRounded;
   }
 
-  // function to calculate the timeFrame input to the entryDate
-  function addDays(date, days) {
-    let result = new Date(date);
-    result.setDate(result.getDate() + days);
-    return result;
-  }
+
   // calling addDays function, assigning result to last day of time frame
   const lastExit = addDays(firstEntryDate, timeFrameValue );
 
